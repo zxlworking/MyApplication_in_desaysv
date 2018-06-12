a9b338c697f0c5595624ad31cd6d3e4af8d4e6de
zxlworking <szzhangxiaolong@163.com>
2018-06-12 16:34:50 +0800

    add calculate project

--- test_cpu/src/main/java/chief/river/zxl/com/test_cpu/GetCpuInfoService.java
+++ test_cpu/src/main/java/chief/river/zxl/com/test_cpu/GetCpuInfoService.java
@@ -14,60 +14,60 @@ import android.widget.Toast;
 
 public class GetCpuInfoService extends Service {
 
-    private static final String TAG = "GetCpuInfoService";
-
-    private static final int MSG_GET_CPU_INFO = 1;
-
-    private CpuTracker mStats = null;
-
-    private Toast mToast = null;
-
-    private Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what){
-                case MSG_GET_CPU_INFO:
-                    mStats.update();
-                    sendEmptyMessageDelayed(MSG_GET_CPU_INFO, 10 * 1000);
-                    break;
-            }
-        }
-    };
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mStats = new CpuTracker(false);
-        mStats.init();
-
-        mHandler.sendEmptyMessageDelayed(MSG_GET_CPU_INFO, 30 * 1000);
-    }
-
+//    private static final String TAG = "GetCpuInfoService";
+//
+//    private static final int MSG_GET_CPU_INFO = 1;
+//
+//    private CpuTracker mStats = null;
+//
+//    private Toast mToast = null;
+//
+//    private Handler mHandler = new Handler() {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            switch (msg.what){
+//                case MSG_GET_CPU_INFO:
+//                    mStats.update();
+//                    sendEmptyMessageDelayed(MSG_GET_CPU_INFO, 10 * 1000);
+//                    break;
+//            }
+//        }
+//    };
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        mStats = new CpuTracker(false);
+//        mStats.init();
+//
+//        mHandler.sendEmptyMessageDelayed(MSG_GET_CPU_INFO, 30 * 1000);
+//    }
+//
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
-
-    private final class CpuTracker extends ProcessCpuTracker {
-
-        public CpuTracker(boolean includeThreads) {
-            super(includeThreads);
-        }
-
-        @Override
-        public void onLoadChanged(float load1, float load5, float load15) {
-            float mCpuPercent = getTotalCpuPercent();
-            if(mCpuPercent >= 95){
-                Log.i(TAG,"onLoadChanged::mCpuPercent = " + mCpuPercent);
-                if(mToast != null){
-                    mToast.cancel();
-                    mToast = null;
-                }
-                if(null == mToast){
-                    mToast = Toast.makeText(GetCpuInfoService.this,getString(com.android.systemui.R.string.cpu_hight_info),Toast.LENGTH_SHORT);
-                }
-                mToast.show();
-            }
-        }
-    }
+//
+//    private final class CpuTracker extends ProcessCpuTracker {
+//
+//        public CpuTracker(boolean includeThreads) {
+//            super(includeThreads);
+//        }
+//
+//        @Override
+//        public void onLoadChanged(float load1, float load5, float load15) {
+//            float mCpuPercent = getTotalCpuPercent();
+//            if(mCpuPercent >= 95){
+//                Log.i(TAG,"onLoadChanged::mCpuPercent = " + mCpuPercent);
+//                if(mToast != null){
+//                    mToast.cancel();
+//                    mToast = null;
+//                }
+//                if(null == mToast){
+//                    mToast = Toast.makeText(GetCpuInfoService.this,getString(com.android.systemui.R.string.cpu_hight_info),Toast.LENGTH_SHORT);
+//                }
+//                mToast.show();
+//            }
+//        }
+//    }
 }
