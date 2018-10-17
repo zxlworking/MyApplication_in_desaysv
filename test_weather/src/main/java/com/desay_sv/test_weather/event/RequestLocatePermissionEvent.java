7f2287980536f896cc5c7b8a8706e6dcb375e3e6
zxlworking <szzhangxiaolong@163.com>
2018-10-17 15:16:24 +0800

    add test weather

--- test_weather/src/main/java/com/desay_sv/test_weather/event/RequestLocatePermissionEvent.java
+++
@@ -1,8 +0,0 @@
-package com.desay_sv.test_weather.event;
-
-/**
- * Created by zxl on 2018/9/6.
- */
-
-public class RequestLocatePermissionEvent {
-}
--- test_weather/src/main/java/com/desay_sv/test_weather/http/data/TodayWeatherHumidityIconCss.java
+++
@@ -1,9 +0,0 @@
-package com.desay_sv.test_weather.http.data;
-
-/**
- * Created by zxl on 2018/9/5.
- */
-
-public class TodayWeatherHumidityIconCss extends TodayWeatherIconCss{
-    
-}
